d77125642a689de2c6fe4a1e81bda5e107681826
Adeyemi Thompson <thompsonadeyemi@gmail.com>
2019-02-11 01:26:16 +0000

    some changes to design of elements

--- js/timeOfDay.js
+++ js/timeOfDay.js
@@ -8,12 +8,15 @@ function checkTimeOfDay(){
     if(theHours >= 0 && theHours < 12){
         greetingTxt = morning;
         document.getElementById("main_wrapper").className = "morning";
+        document.getElementsByClassName("morning")[0].style.opacity = 1;
     }else if(theHours >= 12 && theHours < 17){
         greetingTxt = afternoon;
         document.getElementById("main_wrapper").className = "afternoon";
+        document.getElementsByClassName("afternoon")[0].style.opacity = 1;
     }else if(theHours >= 17 && theHours < 24){
         greetingTxt = evening;
         document.getElementById("main_wrapper").className = "evening";
+        document.getElementsByClassName("evening")[0].style.opacity = 1;
     }
 }
 
