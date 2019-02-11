2722a5880de122cb88c7621af84075ea4b73c7e7
Adeyemi Thompson <thompsonadeyemi@gmail.com>
2019-02-11 19:55:56 +0000

    delete task items in dolist functionality working

--- js/dolistHandler.js
+++ js/dolistHandler.js
@@ -42,7 +42,6 @@ function addNewToDo() {
         //check if localstorage taskItemsArray exists already
         if(localStorage.getItem("taskItemsArray") != null){
             //if so populate blank taskItemsArray variable
-            //taskItemsArray.push(JSON.parse(localStorage.getItem("taskItemsArray")));
             taskItemsArray = JSON.parse(localStorage.getItem("taskItemsArray"));
             console.log("initial taskItemsArray: " + JSON.stringify(taskItemsArray));
         }
@@ -53,12 +52,6 @@ function addNewToDo() {
 
         //save taskItemsArray to localstorage
         localStorage.setItem("taskItemsArray", JSON.stringify(taskItemsArray));
-
-        //display list item to list element
-        //var newItem = "<li id='" + listID + "' class='hoverReveal pos-relative'>" + todoTxt + "<input type='checkbox' id='checkbox" + listID + "' name='checkbox" + listID + "' value=''><label class='" + selValue + "' for='checkbox" + listID + "'><span><!-- Span to be used to create checkbox styling --></span></label><a href='#' class='close' id='close'></li>";
-        //var newItem = "<li id='" + JSON.parse(taskItemsArray[i]).id + "' class='hoverReveal pos-relative'>" + JSON.parse(taskItemsArray[i]).task + "<input type='checkbox' id='checkbox" + taskItemsArray[i]).id + "' name='checkbox" + taskItemsArray[i]).id + "' value=''><label class='" + JSON.parse(taskItemsArray[i]).urgency + "' for='checkbox" + taskItemsArray[i]).id + "'><span><!-- Span to be used to create checkbox styling --></span></label><a href='#' class='close' id='close'></li>";
-        //list.insertAdjacentHTML('beforeend', newItem);
-        //form.reset();
 
         //call displayDoList function
         displayDoList();
@@ -76,14 +69,16 @@ function addNewToDo() {
 function displayDoList(){
     //make tempItemsArray
     var tempItemsArray = JSON.parse(localStorage.getItem("taskItemsArray"));
-    console.log("tempItemsArray: " + tempItemsArray);
+    console.log("tempItemsArray: " + JSON.stringify(tempItemsArray));
+    //console.log("sorted tempItemsArray: " + JSON.stringify(sortByKey(tempItemsArray, "urgency")));
+    tempItemsArray = sortByKey(tempItemsArray, "urgency");
 
     while(list.firstChild){
         list.removeChild(list.firstChild);
     }
 
     if(tempItemsArray != null){
-        //parse through items array with for loop
+        //go through items array with for loop
         for(i = 0; i < tempItemsArray.length; i++){
             //make a newItem variable to hold html markup for each task item
             var newItem = "<li id='" + tempItemsArray[i].id + "' class='hoverReveal pos-relative'>" + tempItemsArray[i].task + "<input type='checkbox' id='checkbox" + tempItemsArray[i].id + "' name='checkbox" + tempItemsArray[i].id + "' value=''><label class='" + tempItemsArray[i].urgency + "' for='checkbox" + tempItemsArray[i].id + "'><span><!-- Span to be used to create checkbox styling --></span></label><a href='#' class='close' id='close'></li>";
@@ -96,4 +91,32 @@ function displayDoList(){
         //do nothing
         console.log("Nothing found in tempItemsArray.")
     }
+}
+
+//function to sort through array by the key of an object
+function sortByKey(array, key){
+    return array.sort(function(a,b){
+        var x = a[key];
+        var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
+//function call to delete an item from dolist
+function deleteByValue(value){
+    console.log("value to delete by: " + value);
+    //make deleteFromItemsArray
+    var deleteFromItemsArray = JSON.parse(localStorage.getItem("taskItemsArray"));
+
+    //go through items array with for loop
+    for(i = 0; i < deleteFromItemsArray.length; i++){
+        if(deleteFromItemsArray[i].id == value){
+            console.log("This is the object to delete here");
+            deleteFromItemsArray.splice(i, 1);
+        }
+    }
+
+    console.log("deleteFromItemsArray after delete: " + JSON.stringify(deleteFromItemsArray));
+    //save taskItemsArray to localstorage
+    localStorage.setItem("taskItemsArray", JSON.stringify(deleteFromItemsArray));
 }
