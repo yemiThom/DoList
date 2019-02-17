66eeacbde8e46a60972cad5b129f324757e2f47a
Adeyemi Thompson <thompsonadeyemi@gmail.com>
2019-02-17 21:25:10 +0000

    dolist_username input functionality update

--- js/timeOfDay.js
+++ js/timeOfDay.js
@@ -9,17 +9,23 @@ function checkTimeOfDay(){
         greetingTxt = morning;
         document.getElementById("main_wrapper").className = "morning";
         document.getElementsByClassName("morning")[0].style.opacity = 1;
-        document.getElementById("welcomeText").innerHTML = morning + ", " + localStorage.getItem("dolist_username") + "! Here are your tasks for today.";
+        if(localStorage.getItem("dolist_username") != null){
+            document.getElementById("welcomeText").innerHTML = morning + ", " + localStorage.getItem("dolist_username") + "! Here are your tasks for today.";
+        }
     }else if(theHours >= 12 && theHours < 17){
         greetingTxt = afternoon;
         document.getElementById("main_wrapper").className = "afternoon";
         document.getElementsByClassName("afternoon")[0].style.opacity = 1;
-        document.getElementById("welcomeText").innerHTML = afternoon + ", " + localStorage.getItem("dolist_username") + "! Here are your tasks for today.";
+        if(localStorage.getItem("dolist_username") != null){
+            document.getElementById("welcomeText").innerHTML = afternoon + ", " + localStorage.getItem("dolist_username") + "! Here are your tasks for today.";
+        }
     }else if(theHours >= 17 && theHours < 24){
         greetingTxt = evening;
         document.getElementById("main_wrapper").className = "evening";
         document.getElementsByClassName("evening")[0].style.opacity = 1;
-        document.getElementById("welcomeText").innerHTML = evening + ", " + localStorage.getItem("dolist_username") + "! Here are your tasks for today.";
+        if(localStorage.getItem("dolist_username") != null){
+            document.getElementById("welcomeText").innerHTML = evening + ", " + localStorage.getItem("dolist_username") + "! Here are your tasks for today.";
+        }
     }
 }
 
